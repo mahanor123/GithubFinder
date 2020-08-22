9f8ea119e45843eeafdf3dbe6bd64eca95231421
Your Name <you@example.com>
2020-08-22 14:37:55 +0530

    first commit

---
+++ src/finder.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Url from './url';
+import Repo from './repo';
+import RepoUrl from './repo_url';
+import { TextField, Paper } from '@material-ui/core';
+class Finder extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { query: '', profile: '', repo: '', public: '' };
+	}
+	handleInputChange = e => {
+		this.setState({
+			query: e.target.value,
+		});
+	};
+	handleSubmit = async e => {
+		e.preventDefault();
+		const response = await Url.get('users', {
+			params: {
+				q: this.state.query,
+			},
+		});
+		const resp = await RepoUrl.get(`/${this.state.query}/repos`);
+		// console.log(response);
+		// this.setState({ repos: response.data.items, rep: resp.data });
+
+		const public_repo = await RepoUrl.get(`/${this.state.query}`);
+		console.log(public_repo.data);
+		this.setState({ profile: response.data.items, repo: resp.data, public: public_repo.data });
+	};
+
+	render() {
+		return (
+			<div>
+				<Paper elevation={6} style={{ padding: '15px', margin: '15px', backgroundColor: 'grey' }}>
+					<form onSubmit={this.handleSubmit}>
+						<TextField fullWidth label="search..." onChange={this.handleInputChange}></TextField>
+					</form>
+				</Paper>
+				{this.state.profile !== '' && (
+					<Repo image={this.state.profile} info_repo={this.state.repo} show_repo={this.state.public} />
+				)}
+			</div>
+		);
+	}
+}
+export default Finder;
---
+++ src/repo.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
+import './App.css';
+class Repo extends React.Component {
+	render() {
+		const { image } = this.props;
+		const { info_repo } = this.props;
+		const { show_repo } = this.props;
+		return (
+			<div>
+				{/* <div style={{display: 'inline-block'}}> */}
+				{image.map(profile => (
+					<Card style={{ width: '260px', marginLeft: '200px', cursor: 'pointer' }}>
+						<CardContent>
+							<img style={{ width: '200px' }} alt="thumbnail" src={profile.avatar_url} />
+							<a href={profile.html_url} style={{ fontSize: '27px', textDecoration: 'none' }}>
+								<Button
+									variant="contained"
+									style={{ fontSize: '27', marginTop: '20px', width: '100%' }}>
+									view
+								</Button>
+							</a>
+							<p style={{ fontSize: '27', color: '#666' }}>{profile.login}</p>
+							<hr />
+							<br />
+							<hr />
+							<span style={{ backgroundColor: '#5bc0de', padding: '3px 5px 3px 5px', fontsize: '0px' }}>
+								{show_repo.public_repos}Public Repo
+							</span>
+							<span style={{ backgroundColor: '#0275d8', padding: '3px 5px 3px 5px' }}>
+								{show_repo.public_gists}Public_Gists
+							</span>
+							<br />
+							<br />
+							<span style={{ backgroundColor: '#d9534f', padding: '3px 15px 5px 15px', fontsize: '0px' }}>
+								{show_repo.followers}Followers
+							</span>
+							<span style={{ backgroundColor: '#373a3c', padding: '3px 15px 5px 15px' }}>
+								{show_repo.following}Following
+							</span>
+						</CardContent>
+					</Card>
+				))}
+
+				{/* <span>{show_repo.public_repos}</span> */}
+				{/* </div> */}
+				<div style={{ marginLeft: '450px', display: 'inline-block', marginTop: '-400px' }}>
+					{info_repo.map(profile => (
+						<Card style={{ width: '425px', display: 'inline-block', marginLeft: '50px' }}>
+							<CardContent>
+								<Typography color="textSecondary" gutterBottom>
+									<a
+										href={profile.html_url}
+										style={{ fontSize: '15px', textDecoration: 'none', display: 'table-cell' }}
+										target="_blank"
+										rel="noopener noreferrer">
+										{profile.name}
+									</a>
+									<p>
+										<a
+											href={profile.html_url}
+											style={{
+												fontSize: '13px',
+												lineHeight: '0',
+												textDecoration: 'none',
+												color: 'black',
+											}}
+											target="_blank"
+											rel="noopener noreferrer">
+											{profile.full_name}
+										</a>
+									</p>
+									<p style={{ fontSize: '13px' }}>{profile.description}</p>
+									<p style={{ fontSize: '15px', display: 'inline-block' }}>{profile.language}</p>
+									<span
+										style={{
+											backgroundColor: '#0275d8',
+											marginLeft: '100px',
+											padding: '3px 10px 3px 10px',
+										}}>
+										{profile.forks_count}Fork
+									</span>
+									<span style={{ backgroundColor: '#d9534f', padding: '3px 10px 3px 10px' }}>
+										{profile.watchers_count}watchers
+									</span>
+									<span style={{ backgroundColor: '#4CAF50 ', padding: '3px 10px 3px 10px' }}>
+										{profile.stargazers_count}star
+									</span>
+									<p>{profile.created_at}</p>
+								</Typography>
+							</CardContent>
+						</Card>
+					))}
+				</div>
+			</div>
+		);
+	}
+}
+export default Repo;
